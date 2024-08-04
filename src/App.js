cb86b5ac3d6fc9d06ad5521053a2c56158a49f60
Rupali Harjai <harjairupali@gmai.com>
2024-08-04 22:46:25 +0530

    Add src folder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-slate-500 decoration-double uppercase decoration-8 font-bold text-9xl">
+     <h3>jiiiiiii</h3>
+    </div>
+  );
+}
+
+export default App;
