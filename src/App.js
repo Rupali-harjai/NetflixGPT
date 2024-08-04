0043df5dbe139d09982e75302c7218a4a8274cb4
Rupali harjai <harjairupali@gmail.com>
2024-08-04 23:48:59 +0530

    Add logo and paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-slate-500 decoration-double uppercase decoration-8 font-bold text-9xl">
-     <h3>jiiiiiii</h3>
-    </div>
+  <Body/>
   );
 }
 
